523bc8daaa32beec3d19acabc2b6e270f9061d69
d-ava <d.avalishvili@gmail.com>
2021-10-03 12:46:24 +0400

    changes in adapter.
    interface OnUserClicklListener
    
    class UserViewHolder OnClickListener was implemented
    override onClick
    
    MainActivity onUserClickListener implemented
    override onUserClick

--- app/src/main/java/com/example/assignment_003/MainActivity.kt
+++ app/src/main/java/com/example/assignment_003/MainActivity.kt
@@ -3,37 +3,43 @@ package com.example.assignment_003
 import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.renderscript.ScriptGroup
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.assignment_003.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : AppCompatActivity(), UserAdapter.OnUserClickListener{
 
 
     lateinit var binding: ActivityMainBinding
-    val data = mutableListOf<UserData>(
+    private val data = mutableListOf<UserData>(
 
-        UserData("John", "Connor", "astalavista@baby.com" ),
-                UserData("John", "Travolta", "john@travolta.com" )
-
+        UserData("John", "Connor", "astalavista@baby.com"),
+        UserData("John", "Travolta", "john@travolta.com"),
+        UserData("Roger", "Waters", "mail1@mail.com"),
+        UserData("Indiana", "jones", "mail2@mail.com"),
+        UserData("Sir", "nickolson", "mail3@mail.com"),
+        UserData("john", "lee", "mail4@mail.com"),
+        UserData("bondo", "cage", "mail5@mail.com"),
+        UserData("london", "rogers", "mail6@mail.com"),
     )
 
-    private val adapter = UserAdapter(data)
+    private val adapter = UserAdapter(data, this)
     private val layoutManager = LinearLayoutManager(this)
-
-
-
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        binding= ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.recyclerView.layoutManager = layoutManager
         binding.recyclerView.adapter = adapter
+
+
+
 
         val editProfileLauncher =
             registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
@@ -50,18 +56,27 @@ class MainActivity : AppCompatActivity() {
 
 
 
+
+
+
+
+
+
+
     }
 
+    override fun onUserClick(position: Int) {
 
+       val clickedUser: UserData = data[position]
+        Toast.makeText(this, "# $position name is ${clickedUser.name}  ", Toast.LENGTH_LONG).show()
+        // blabla
+    }
 
 
     companion object {
         const val USER1 = "111"
 
     }
-
-
-
 
 
 }
--- app/src/main/java/com/example/assignment_003/UserAdapter.kt
+++ app/src/main/java/com/example/assignment_003/UserAdapter.kt
@@ -1,24 +1,49 @@
 package com.example.assignment_003
 
+import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.assignment_003.databinding.UserItemBinding
 
-class UserAdapter(private val dataSet:
-                  MutableList<UserData>) :
+class UserAdapter(
+    private val dataSet: MutableList<UserData>,
+    private val listener: OnUserClickListener
+) :
     RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
 
 
-
     // Create new views (invoked by the layout manager)
+    //class UserViewHolder(val binding: UserItemBinding) : RecyclerView.ViewHolder(binding.root)
 
-    class UserViewHolder(val binding: UserItemBinding) : RecyclerView.ViewHolder(binding.root)
+    inner class UserViewHolder(val binding: UserItemBinding) :
+        RecyclerView.ViewHolder(binding.root),
+        View.OnClickListener {
+
+        init {
+            binding.root.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View?) {
+            val position = adapterPosition
+            if (position != RecyclerView.NO_POSITION) {
+                listener.onUserClick(position)
+            }
+        }
+    }
+
+
+    interface OnUserClickListener {
+        fun onUserClick(position: Int)
+    }
 
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): UserViewHolder {
         // Create a new view, which defines the UI of the list item
-        val binding = UserItemBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false)
+        val binding =
+            UserItemBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false)
 
 
         return UserViewHolder(binding)
@@ -29,12 +54,10 @@ class UserAdapter(private val dataSet:
 
         val userItem = dataSet[position]
         viewHolder.binding.tvName.text = userItem.name
-       viewHolder.binding.tvLastName.text = userItem.lastName
+        viewHolder.binding.tvLastName.text = userItem.lastName
         viewHolder.binding.email.text = userItem.email
 
-        viewHolder.binding.root.setOnClickListener {
 
-        }
 
 
     }
